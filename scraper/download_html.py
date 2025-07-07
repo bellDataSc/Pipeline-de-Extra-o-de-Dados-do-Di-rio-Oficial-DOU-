ac18542d88a53386073d905f8261783d93e1eca9
Isabel Cruz <93488292+bellDataSc@users.noreply.github.com>
2025-07-07 10:28:11 -0300

    update library

--- scraper/download_html.py
+++ scraper/download_html.py
@@ -8,6 +8,6 @@ def baixar_html_data(data=date.today().strftime('%Y-%m-%d')):
     if response.status_code == 200:
         with open(f"data/diarios_html/dou_{data}.html", "w", encoding="utf-8") as f:
             f.write(response.text)
-        print(f"✅ Página {data} salva.")
+        print(f"Página {data} salva.")
     else:
-        print(f"❌ Erro ao baixar página: {response.status_code}")
+        print(f"Erro ao baixar página: {response.status_code}")
